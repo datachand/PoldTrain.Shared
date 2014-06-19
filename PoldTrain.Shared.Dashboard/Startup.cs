6499de20366c342c8d0bf76d2d254de51f546d8c
Data Chand <data.chand@gmail.com>
2014-06-19 15:17:38 +0530

    Initial Commit

---
+++ PoldTrain.Shared.Dashboard/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PoldTrain.Shared.Dashboard.Startup))]
+namespace PoldTrain.Shared.Dashboard
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ PoldTrain.Shared.Logger/Logger.cs
@@ -0,0 +1,21 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoldTrain.Shared.Logger
+{
+    class Logger
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Logger));
+
+        public Logger()
+        {
+
+        }
+
+
+    }
+}
